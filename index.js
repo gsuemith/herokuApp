47566dfa9d795f6e26c1793f5dab51c1829be409
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-08 13:52:26 -0400

    made client

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 9000
+const path = require('path')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "client/build")))
 
 app.use("/api/", (_, res) => {
   res.json({ data: "API is accounted for" })
